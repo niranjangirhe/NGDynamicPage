915451e3c09b7d5914ae869a00a19473bffbe8b8
niranjangirhe <niranjangirheindia@gmail.com>
2021-09-18 18:47:10 +0530

    Json format updated

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 var jsonobj;
 var intenturl = window.location.href;
 console.log(intenturl);
+var btn1="";
+var btn2="";
 LoadFirst()
 function Get(yourUrl) {
     var Httpreq = new XMLHttpRequest(); // a new request
@@ -8,10 +10,19 @@ function Get(yourUrl) {
     Httpreq.send(null);
     console.log(Httpreq.responseText+"done")
     jsonobj = JSON.parse(Httpreq.responseText);
-    document.getElementById("version").innerHTML = jsonobj[0].version
-    var changelog = jsonobj[0].changelog
-    changelog = changelog.substring(changelog.indexOf("*"));
-    document.getElementById("changelog").innerHTML = changelog.replaceAll("\n", "<br>")
+    document.getElementById("title1").innerHTML = jsonobj.title1
+    document.getElementById("title2").innerHTML = jsonobj.title2
+    document.getElementById("title3").innerHTML = jsonobj.title3
+    document.getElementById("para1").innerHTML = jsonobj.para1
+    document.getElementById("para2").innerHTML = jsonobj.para2
+    document.getElementById("para3").innerHTML = jsonobj.para3
+    document.getElementById("heading2").innerHTML = jsonobj.heading2
+    document.getElementById("subtitle2").innerHTML = jsonobj.subtitle2
+    document.getElementById("subtitle3").innerHTML = jsonobj.subtitle3
+    console.log("hi",jsonobj.title1,jsonobj.title2,jsonobj.title3,jsonobj.para1,jsonobj.para2,jsonobj.para3,jsonobj.heading1,jsonobj.subtitle2,jsonobj.subtitle3)
+    btn1=jsonobj.btn1
+    btn2=jsonobj.btn2
+    btn3=jsonobj.btn3
 }
 function LoadFirst(){
 
