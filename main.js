95f8848b530eaece5e3166d82914cc3643ba59ad
niranjangirhe <niranjangirheindia@gmail.com>
2021-09-18 18:56:44 +0530

    Implemented dynamic step 1

--- main.js
+++ main.js
@@ -13,13 +13,16 @@ function Get(yourUrl) {
     document.getElementById("title1").innerHTML = jsonobj.title1
     document.getElementById("title2").innerHTML = jsonobj.title2
     document.getElementById("title3").innerHTML = jsonobj.title3
+    document.getElementById("btn1").innerHTML = jsonobj.btn1
+    document.getElementById("btn2").innerHTML = jsonobj.btn2
     document.getElementById("para1").innerHTML = jsonobj.para1
     document.getElementById("para2").innerHTML = jsonobj.para2
     document.getElementById("para3").innerHTML = jsonobj.para3
     document.getElementById("heading2").innerHTML = jsonobj.heading2
     document.getElementById("subtitle2").innerHTML = jsonobj.subtitle2
     document.getElementById("subtitle3").innerHTML = jsonobj.subtitle3
-    console.log("hi",jsonobj.title1,jsonobj.title2,jsonobj.title3,jsonobj.para1,jsonobj.para2,jsonobj.para3,jsonobj.heading1,jsonobj.subtitle2,jsonobj.subtitle3)
+    document.getElementById("img").src = jsonobj.img
+    console.log("hi",jsonobj.title1,jsonobj.title2,jsonobj.title3,jsonobj.para1,jsonobj.para2,jsonobj.para3,jsonobj.heading2,jsonobj.subtitle2,jsonobj.subtitle3)
     btn1=jsonobj.btn1
     btn2=jsonobj.btn2
     btn3=jsonobj.btn3
